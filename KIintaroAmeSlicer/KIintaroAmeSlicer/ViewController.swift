ae0e524e575a4aec181832a204dfc1a1849e7857
kamui00002 <kamui00002@yahoo.co.jp>
2025-10-15 23:15:58 +0900

    ほぼ完成系

---
+++ KIintaroAmeSlicer/KIintaroAmeSlicer/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
